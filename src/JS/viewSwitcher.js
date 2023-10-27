873b5d7bba036a8f44de2b24ac2e46b59f8a8d9b
Torstein <141224356+Trind20@users.noreply.github.com>
2023-10-27 10:12:19 +0200

    Update viewSwitcher.js

--- src/JS/viewSwitcher.js
+++ src/JS/viewSwitcher.js
@@ -36,11 +36,10 @@ function updateView(){
             model.app.pageOrigin = "myBirds";
             break;
 
-        case "birdInfo":
+        default:
             updateBirdInfoView()
             swap("./src/CSS/birdInfoPage.css","Fugle info","src/Picture/bird_Info.jpg")
             break;
-            // default: throw "ERR";
     }
 }
 
