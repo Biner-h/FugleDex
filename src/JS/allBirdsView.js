36acedcd74d79e00b7f274fa9254dd65a24824ce
GitGetAc <Git.Zoltan.Ladanyi@gmx.com>
2023-11-01 10:47:15 +0100

    Filtrering, sortering allbirdsview, mybirdsview
    Filtrering, sortering allbirdsview, mybirdsview

--- src/JS/allBirdsView.js
+++ src/JS/allBirdsView.js
@@ -1,5 +1,3 @@
-"use strict"; 
-
 /** Start page */
 updateView()
 
@@ -11,10 +9,10 @@ function updateAllBirdsView(){
                 <div onmouseover="run('Dropped')" onclick="showDropDown()" class="sorterKnapp">Sorter</div>
                 <div id="dropDown" class="dropdown-content">
                 <p class="sorterEtter">Sorter etter:</p>
-                <p class="sorterFilter" onclick="vekt();">Vekt</p>
-                <p class="sorterFilter" onclick="fart();">Fart</p>
-                <p class="sorterFilter" onclick="size();">Størrelse</p>
-                <p class="sorterFilter" onclick="lifespan();">Levetid</p>
+                <p class="sorterFilter" onclick="setSort('vekt')">Vekt</p>
+                <p class="sorterFilter" onclick="setSort('fart')">Fart</p>
+                <p class="sorterFilter" onclick="setSort('size')">Størrelse</p>
+                <p class="sorterFilter" onclick="setSort('lifespan')">Levetid</p>
             </div>
         </div> 
         <div class="home" onclick="fortsett('home')">Hjem</div>
@@ -25,14 +23,15 @@ function updateAllBirdsView(){
 
 function showBirds(model)
 {
+    
     let div = "";
-    for(let i = 0; i < model.data.birds.length; i++)
+    for(let i = 0; i < model.app.sortedList.length; i++)
     {
         div += `<div onclick="showBirdInfo(${i})" 
-        onmouseover="run('${model.data.birds[i].bird_Name}'); 
-        "${model.data.birds[i].found ? 'class="birdFound box"' : 'class="birdNotFound box"'}>
-        <img src="${model.data.birds[i].image}">
-        <h2 id="birdName">${model.data.birds[i].bird_Name}</h2>
+        onmouseover="run('${model.app.sortedList[i].bird_Name}'); 
+        "${model.app.sortedList[i].found ? 'class="birdFound box"' : 'class="birdNotFound box"'}>
+        <img src="${model.app.sortedList[i].image}">
+        <h2 id="birdName">${model.app.sortedList[i].bird_Name}</h2>
         </div>`;
     }
     return div;
--- src/JS/controller.js
+++ src/JS/controller.js
@@ -1,42 +1,59 @@
 "use strict";
 
 /* All Birds */
-    
+initList();
+
+
+////////////////////////////Sortering////////////////////////////////////////
+
+function sortBirds(){
+    switch (model.app.sortMethod) {
+        case "vekt":
+            vekt()
+            break;
+        case "fart":
+            fart()
+            break;
+        case "size":
+            size()
+            break;
+        case "lifespan":
+            lifespan()
+            break;
+    }
+}
+
+function setSort(sortType){
+    model.app.sortMethod = sortType
+    sortBirds()
+    updateView()
+}
+
 const vekt = () =>
 {
-    
+    model.app.sortedList = [...model.app.sortedList.sort((a,b) => b.we - a.we)]
 }
 
 const fart = () =>
 {
-    let ret;
-    let model_Sorted = 
-    {
-        data: 
-        {
-            birds:
-            [
-                model.data.birds[6],
-                model.data.birds[1],
-                model.data.birds[3],
-            ]
-        }
-    }
-    birds = showBirds(model_Sorted);
-
-    return;
+    model.app.sortedList = [...model.app.sortedList.sort((a,b) => b.speed - a.speed)]
 }
 const size = () => 
 {
-
+    model.app.sortedList = [...model.app.sortedList.sort((a,b) => b.si - a.si)]
 }
+
+const lifespan = () =>
+{
+    model.app.sortedList = [...model.app.sortedList.sort((a,b) => b.li - a.li)]
+}
+///////////////////////////////////////////////////////////////////////////////
 
 function validator()
 {
     for(let i = 0; i != model.data.accounts.length; i++)
     {
-        if(model.data.accounts[i].user_Name == model.inputs.loginPage.user_Name && 
-            model.data.accounts[i].password == model.inputs.loginPage.password)
+        if(model.data.accounts[i].user_Name == model.inputs.loginPage.user_Name && model.data.accounts[i].password == model.inputs.loginPage.password)
         {
             fortsett("home");
             model.app.loggedInAs.name
@@ -48,16 +65,7 @@ function validator()
             return;
         }
     }
-}
 
-function reg_Push()
-{
-    
-}
-
-const lifespan = () =>
-{
-    
 }
 
 
@@ -66,6 +74,8 @@ function fortsett(param)
     model.app.currentPage = param
     updateView()
 }
+
+
 
 const run = (bird)=>
 {tracker.push(bird);console.log("Bird tracker " + bird);}
@@ -88,4 +98,8 @@ const swap = (csslinker,title,link) =>
     console.log(linker + " " + csslinker)
     console.log(model.app.pageOrigin);
     console.log(model.app.currentPage);
+}
+
+function initList() {
+    model.app.sortedList = [...model.data.birds]
 }
